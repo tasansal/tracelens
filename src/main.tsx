b391a7f6bb1c9e5624c3ba701e9e6580dfb3dbc8
Altay Sansal <tasansal@users.noreply.github.com>
2026-02-01 10:48:14 -0600

    refactor(core): switch to named import for App in main entrypoint

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
  * Client entrypoint that mounts the React app and global styles.
  */
 import ReactDOM from 'react-dom/client';
-import App from './app/App';
+import { App } from './app/App';
 import './index.css';
 
 // Mount the app once the root element is available.
